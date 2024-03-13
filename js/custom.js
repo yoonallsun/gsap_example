03d06a9d42d11a54448b4f2e0ad23e26e6bc0fd5
DH <DH@macmac.skbroadband>
2024-03-13 17:47:27 +0900

    work section...

--- js/custom.js
+++ js/custom.js
@@ -51,6 +51,11 @@ document.addEventListener('DOMContentLoaded', function () {
       start: 'top bottom',
       scrub: 1.8,
     },
+    benefits: {
+      trigger: '.benefits_lists',
+      start: 'top bottom',
+      scrub: 1.8,
+    },
   };
 
   // square rotate animation
@@ -136,7 +141,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
     benefits_nums.forEach((num) => {
       const data_speed = num.getAttribute('data-speed');
-      console.log(data_speed);
+      // console.log(1 - parseFloat(data_speed));
+      
+      tl.from(num, {
+        scrollTrigger: commonScrollTrigger.benefits,
+        x: -data_speed,
+      });
     });
   }
 
